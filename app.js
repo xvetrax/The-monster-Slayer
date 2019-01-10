1ab94e21c835467028ca1d20ff049273d6154425
Andrius <vet.andrius@gmail.com>
2019-01-10 15:11:57 +0200

    add new file, app.js

---
+++ app.js
@@ -0,0 +1,58 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerHealt: 100,
+        monsterHealt: 100,
+        gameIsRunning: false
+    },
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealt = 100;
+            this.monsterHealt = 100;
+        },
+        attack: function() {
+            
+            this.monsterHealt -= this.calculateDamage(3, 10);
+            
+            if (this.checkWin() ){
+                return;
+            }
+             
+            this.playerHealt -= this.calculateDamage(5, 12);
+
+            this.checkWin();
+        },
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
+        },
+        calculateDamage: function(min, max) {
+            return  damage = Math.max(Math.floor(Math.random() + max) + 1, min);
+        },
+
+        checkWin: function() {
+            if (this.monsterHealt <= 0) {
+                if (confirm('You Won! New Game?')) {
+                    this.startGame();
+                }else {
+                    this.gameIsRunning = false;
+                }
+              return true;
+            } else if (this.playerHealt <= 0) {
+                if (confirm('You Lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+});
